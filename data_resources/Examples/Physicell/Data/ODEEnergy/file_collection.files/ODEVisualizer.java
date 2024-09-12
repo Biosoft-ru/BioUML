d7d96faa8bf754c9bd80041e4560e08a2aa8d910
Ilya <damagedone@yandex.ru>
2024-09-12 16:11:52 +0400

    Visualizer in example fixed

--- data_resources/Examples/Physicell/Data/ODEEnergy/file_collection.files/ODEVisualizer.java
+++ data_resources/Examples/Physicell/Data/ODEEnergy/file_collection.files/ODEVisualizer.java
@@ -8,32 +8,34 @@ public class ODEVisualizer extends FalseCellCytometryVisualizer
     @Override
     public Color[] findColors(Cell pCell)
     {
-        int energy_vi = pCell.customData.findVariableIndex( "intra_energy" );
-        Color[] output = super.findColors( pCell );
-        // proliferative cell
-        if( pCell.type == 0 )
+        try
         {
-            if( !pCell.phenotype.death.dead && pCell.customData.get( energy_vi ) > 445 )
-            {
-                output[0] = new Color( 255, 255, 0 );
-                output[2] = new Color( 125, 125, 0 );
-            }
-            // arrested cell
-            if( !pCell.phenotype.death.dead && pCell.customData.get( energy_vi ) <= 445 )
-            {
-                output[0] = new Color( 255, 0, 0 );
-                output[2] = new Color( 125, 0, 0 );
-            }
-            // dead cell
-            if( pCell.phenotype.death.dead )
-            {
-                output[0] = new Color( 20, 20, 20 );
-                output[2] = new Color( 10, 10, 10 );
-            }
-        }
-
-            if (pCell.phenotype.death.dead)
- return new Color[]{new Color(255, 0, 0)};
-        return output;
+           double energy = pCell.phenotype.intracellular.getParameterValue( "$Intracellular.Energy" );
+           Color[] output = super.findColors( pCell );
+           if( pCell.type == 0 )
+           {
+               // dead cell
+               if( pCell.phenotype.death.dead )
+               {
+                   output[0] = new Color( 20, 20, 20 );
+                   output[2] = new Color( 10, 10, 10 );
+               }
+               else  if( energy <= 445 )
+               {
+                   output[0] = new Color( 255, 0, 0 );
+                   output[2] = new Color( 125, 0, 0 );
+               }
+               else
+               {
+                   output[0] = new Color( 255, 255, 0 );
+                   output[2] = new Color( 125, 125, 0 );
+               }
+          }
+          return output;
+       }
+       catch (Exception ex)
+       {
+            return new Color[]{new Color(255, 0, 0)};
+       }
     }
 }
