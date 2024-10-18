490de14c227989f17428f3f59a4a1ad673518d54
Ilya <damagedone@yandex.ru>
2024-10-18 15:36:44 +0400

    readonly removed for web

--- src/biouml/plugins/physicell/web/PhysicellWebTableResolver.java
+++ src/biouml/plugins/physicell/web/PhysicellWebTableResolver.java
@@ -842,7 +842,7 @@ public class PhysicellWebTableResolver extends TableResolver
             property( "type" ).tags( bean -> bean.getTypes() ).add();
             property( "signal" ).tags( bean -> bean.getSignals() ).add();
             property( "color1" ).tags( bean -> bean.getColorSchemes() ).add();
-            property( "color2" ).tags( bean -> bean.getColorSchemes() ).readOnly( "isOneColor" ).add();
+            property( "color2" ).tags( bean -> bean.getColorSchemes() ).add();
             add( "min" );
             add( "max" );
         }
