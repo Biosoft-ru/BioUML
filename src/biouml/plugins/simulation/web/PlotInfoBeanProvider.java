9777b9cef9215c675a4cb13b1db2480de15de5a4
Ilya <damagedone@yandex.ru>
2024-11-10 13:24:32 +0400

    some diagrams do not have EModel

--- src/biouml/plugins/simulation/web/PlotInfoBeanProvider.java
+++ src/biouml/plugins/simulation/web/PlotInfoBeanProvider.java
@@ -1,6 +1,7 @@
 package biouml.plugins.simulation.web;
 
 import biouml.model.Diagram;
+import biouml.model.Role;
 import biouml.model.dynamics.EModel;
 import biouml.model.dynamics.plot.PlotsInfo;
 import ru.biosoft.access.BeanProvider;
@@ -12,20 +13,25 @@ public class PlotInfoBeanProvider implements BeanProvider
     @Override
     public PlotsInfo getBean(String path)
     {
-        Diagram diagram = WebDiagramsProvider.getDiagram(path, true);
+        Diagram diagram = WebDiagramsProvider.getDiagram( path, true );
         if( diagram == null )
             return null;
 
-        Object plotObject = diagram.getAttributes().getValue("Plots");
+        Object plotObject = diagram.getAttributes().getValue( "Plots" );
         if( plotObject instanceof PlotsInfo )
         {
             return (PlotsInfo)plotObject;
         }
         else
         {
-            PlotsInfo plot = new PlotsInfo(diagram.getRole(EModel.class));
-            diagram.getAttributes().add(DPSUtils.createHiddenReadOnlyTransient("Plots", PlotsInfo.class, plot));
-            return plot;
+            Role role = diagram.getRole();
+            if( role instanceof EModel )
+            {
+                PlotsInfo plot = new PlotsInfo((EModel)role);
+                diagram.getAttributes().add( DPSUtils.createHiddenReadOnlyTransient( "Plots", PlotsInfo.class, plot ) );
+                return plot;
+            }
         }
+        return null;
     }
 }
