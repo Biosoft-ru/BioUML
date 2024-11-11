52730d16b2fe4a727f7796120fd88d588b872783
anna <anna@developmentontheedge.com>
2024-11-11 22:42:06 +0700

    Sbol: show glyphs in compartments - draft

--- src/biouml/plugins/sbol/SbolDiagramReader.java
+++ src/biouml/plugins/sbol/SbolDiagramReader.java
@@ -1,16 +1,25 @@
 package biouml.plugins.sbol;
 
+import java.awt.Dimension;
+import java.awt.Point;
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
+import org.sbolstandard.core2.Component;
 import org.sbolstandard.core2.ComponentDefinition;
+import org.sbolstandard.core2.FunctionalComponent;
+import org.sbolstandard.core2.ModuleDefinition;
 import org.sbolstandard.core2.SBOLDocument;
 import org.sbolstandard.core2.SBOLReader;
 import org.sbolstandard.core2.SBOLValidationException;
+import org.sbolstandard.examples.Sbol2Terms.component;
 
 import com.developmentontheedge.beans.DynamicProperty;
 
+import biouml.model.Compartment;
 import biouml.model.Diagram;
 import biouml.model.Node;
 import biouml.standard.type.DiagramInfo;
@@ -44,16 +53,76 @@ public class SbolDiagramReader
 
     private static void fillDiagramByDocument(SBOLDocument doc, Diagram diagram)
     {
+        Map<String, SbolBase> kernels = new HashMap<>();
+        Set<ModuleDefinition> mds = doc.getRootModuleDefinitions();
         Set<ComponentDefinition> components = doc.getComponentDefinitions();
         for ( ComponentDefinition cd : components )
         {
             SbolBase base = new SbolBase(cd);
-            Node node = new Node(diagram, base);
-            node.setUseCustomImage(true);
-            String icon = SbolUtil.getSbolImagePath(cd);
-            node.getAttributes().add(new DynamicProperty("node-image", URL.class, SbolDiagramReader.class.getResource("resources/" + icon + ".png")));
-            diagram.put(node);
+            kernels.put(cd.getName(), base);
         }
+        for ( ModuleDefinition md : mds )
+        {
+            Set<FunctionalComponent> fcs = md.getFunctionalComponents();
+            for ( FunctionalComponent fc : fcs )
+            {
+                ComponentDefinition cd = fc.getDefinition();
+                parseComponentDefinition(cd, diagram, kernels);
+            }
+        }
+        //parseComponentDefinitions(components, diagram, kernels);
+    }
+
+    private static void parseComponentDefinitions(Set<ComponentDefinition> cds, Diagram diagram, Map<String, SbolBase> kernels)
+    {
+        for ( ComponentDefinition cd : cds )
+        {
+            parseComponentDefinition(cd, diagram, kernels);
+        }
+    }
+
+    private static void parseComponentDefinition(ComponentDefinition cd, Diagram diagram, Map<String, SbolBase> kernels)
+    {
+        Set<Component> components = cd.getComponents();
+        if ( !components.isEmpty() )
+        {
+            //Fill as compartment
+            Compartment compartment = new Compartment(diagram, kernels.get(cd.getName()));
+            compartment.setShapeSize(new Dimension(48 * components.size() + 10, 48 + 10));
+            int lX = 0, lY = 20;
+            Point location = new Point(lX, lY);
+            compartment.setLocation(location);
+            //            compartment.setUseCustomImage(true);
+            //            String icon = SbolUtil.getSbolImagePath(cd);
+            //            compartment.getAttributes()
+            //                    .add(new DynamicProperty("node-image", URL.class, SbolDiagramReader.class.getResource("resources/" + icon + ".png")));
+            int i = 0;
+            for ( Component component : components )
+            {
+
+                ComponentDefinition cdNode = component.getDefinition();
+                SbolBase base = kernels.get(cdNode.getName());
+                if ( base != null )
+                {
+                    Node node = new Node(diagram, base);
+                    node.setUseCustomImage(true);
+                    Point nodeLocation = new Point(lX + 48 * (i++), lY + 5);
+                    node.setLocation(nodeLocation);
+                    String icon = SbolUtil.getSbolImagePath(cdNode);
+                    node.getAttributes()
+                            .add(new DynamicProperty("node-image", URL.class, SbolDiagramReader.class.getResource("resources/" + icon + ".png")));
+                    compartment.put(node);
+                }
+            }
+            diagram.put(compartment);
+        }
+        //TODO: if only DNA chain is saved without any elements
+        //TODO: check what types can be compartments (linear, circular DNA, RNA) 
+    }
+
+    private static void fillCompartment()
+    {
+
     }
 
 }
--- src/biouml/plugins/sbol/SbolDiagramViewBuilder.java
+++ src/biouml/plugins/sbol/SbolDiagramViewBuilder.java
@@ -1,17 +1,26 @@
 package biouml.plugins.sbol;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 
+import biouml.model.Compartment;
 import biouml.model.DefaultDiagramViewBuilder;
+import biouml.model.DiagramViewOptions;
 import biouml.model.Node;
+import ru.biosoft.graphics.BoxView;
+import ru.biosoft.graphics.Brush;
 import ru.biosoft.graphics.CompositeView;
 import ru.biosoft.graphics.ImageView;
+import ru.biosoft.graphics.LineView;
+import ru.biosoft.graphics.Pen;
 import ru.biosoft.util.IconUtils;
 
 public class SbolDiagramViewBuilder extends DefaultDiagramViewBuilder
@@ -48,6 +57,25 @@ public class SbolDiagramViewBuilder extends DefaultDiagramViewBuilder
         cView.setLocation(node.getLocation());
         node.setView(cView);
         return cView;
+    }
+
+    @Override
+    public boolean createCompartmentCoreView(CompositeView container, Compartment compartment, DiagramViewOptions options, Graphics g)
+    {
+        Dimension shapeSize = compartment.getShapeSize();
+        BoxView shapeView = new BoxView(null, getBrush(compartment, new Brush(Color.yellow)),
+                new Rectangle(0, 0, shapeSize.width, shapeSize.height));
+        shapeView.setLocation(compartment.getLocation());
+        container.add(shapeView);
+        // create compartment shape view
+        Pen boldPen = new Pen(2, Color.black);
+        LineView compView = new LineView(boldPen, 0, 0, (float) compartment.getShapeSize().getWidth(), 0);
+        container.add(compView, CompositeView.X_CC | CompositeView.Y_BB, new Point(0, 5));
+        container.setModel(compartment);
+
+        compView.setModel(compartment);
+        compView.setActive(true);
+        return false;
     }
 
 }
