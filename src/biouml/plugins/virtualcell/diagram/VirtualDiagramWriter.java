9c26ef0e7583c6b22abda00796c9f76c8590d918
Ilya <damagedone@yandex.ru>
2024-11-21 11:03:52 +0400

    import fixed

--- src/biouml/plugins/virtualcell/diagram/VirtualDiagramWriter.java
+++ src/biouml/plugins/virtualcell/diagram/VirtualDiagramWriter.java
@@ -2,6 +2,7 @@ package biouml.plugins.virtualcell.diagram;
 
 import biouml.model.util.DiagramXmlWriter;
 import biouml.model.util.ModelXmlWriter;
+import biouml.plugins.physicell.PhysicellModelWriter;
 
 public class VirtualDiagramWriter extends DiagramXmlWriter
 {
