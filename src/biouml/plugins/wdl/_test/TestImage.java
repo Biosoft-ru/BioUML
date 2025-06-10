95cf8a01b1bd072ab646b46b25dc51035dd41a78
Ilya <damagedone@yandex.ru>
2025-06-11 01:57:26 +0400

    test updated

--- src/biouml/plugins/wdl/_test/TestImage.java
+++ src/biouml/plugins/wdl/_test/TestImage.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
 import com.developmentontheedge.application.ApplicationUtils;
 
 import biouml.model.Diagram;
-import biouml.plugins.wdl.diagram.WDLDiagramTransformer;
+import biouml.plugins.wdl.WDLGenerator;
 import biouml.plugins.wdl.diagram.WDLDiagramType;
 import biouml.plugins.wdl.diagram.WDLImporter;
 import biouml.plugins.wdl.parser.AstBashString;
@@ -23,6 +23,7 @@ import biouml.plugins.wdl.parser.AstRuntime;
 import biouml.plugins.wdl.parser.AstStart;
 import biouml.plugins.wdl.parser.AstSymbol;
 import biouml.plugins.wdl.parser.AstTask;
+import biouml.plugins.wdl.parser.AstVersion;
 import biouml.plugins.wdl.parser.AstWorkflow;
 import biouml.plugins.wdl.parser.SimpleNode;
 import biouml.plugins.wdl.parser.WDLParser;
@@ -31,79 +32,86 @@ public class TestImage
 {
 
     private static String name = "Example";
-//    private static String path = "C:/Users/Damag/eclipse_2024_6/biouml_wdl/test_examples/hello.wdl";
     private static String path = "C:/Users/Damag/eclipse_2024_6/biouml_wdl/test_examples/hello.wdl";
     private static Diagram diagram;
 
     public static void main(String ... args) throws Exception
     {
-        diagram = new WDLDiagramType().createDiagram( null, name, null );
         WDLParser parser = new WDLParser();
-        String wdl = ApplicationUtils.readAsString(  new File( path ) );
-        wdl = wdl.replace("<<<", "{").replace( ">>>", "}" );
-        AstStart start = parser.parse( new StringReader(wdl) );
-        new Visitor().visitNode( start );
+        String wdl = ApplicationUtils.readAsString( new File( path ) );
+        wdl = wdl.replace( "<<<", "{" ).replace( ">>>", "}" );
+        AstStart start = parser.parse( new StringReader( wdl ) );
+        WDLImporter importer = new WDLImporter();
+        diagram = importer.generateDiagram( start, null, name );
+
+        WDLGenerator generator = new WDLGenerator();
+        String s = generator.generateWDL( diagram );
+        System.out.println( "Rexported WDL: " );
+        System.out.println( s);
     }
-    
+
     public static class Visitor
     {
-        public  void visitNode(SimpleNode node)
+        public void visitNode(SimpleNode node)
         {
-            
-            if (node instanceof AstTask)
+            if( node instanceof AstVersion )
             {
-                AstTask taskNode = ((AstTask)node);
+                System.out.println( ( (AstVersion)node ).jjtGetLastToken() );
+            }
+            else if( node instanceof AstTask )
+            {
+                AstTask taskNode = ( (AstTask)node );
                 String name = taskNode.getName();
-                System.out.println( "TASK " + name);       
+                System.out.println( "TASK " + name );
             }
-            else if (node instanceof AstInput)
+            else if( node instanceof AstInput )
             {
-                AstInput inputNode = ((AstInput)node);
-                System.out.println( "INPUT "); 
+                AstInput inputNode = ( (AstInput)node );
+                System.out.println( "INPUT " );
             }
-            else if (node instanceof AstOutput)
+            else if( node instanceof AstOutput )
             {
-                AstOutput outputNode = ((AstOutput)node);
-                System.out.println( "OUTPUT "); 
+                AstOutput outputNode = ( (AstOutput)node );
+                System.out.println( "OUTPUT " );
             }
-            else if (node instanceof AstDeclaration)
+            else if( node instanceof AstDeclaration )
             {
-                AstDeclaration declarationNode = ((AstDeclaration)node);
-                System.out.println( "DECLARATION "+declarationNode.toString()); 
+                AstDeclaration declarationNode = ( (AstDeclaration)node );
+                System.out.println( "DECLARATION " + declarationNode.toString() );
             }
-            else if (node instanceof AstCommand)
+            else if( node instanceof AstCommand )
             {
-                AstCommand commandNode = ((AstCommand)node);
-                System.out.println( "COMMAND "); 
+                AstCommand commandNode = ( (AstCommand)node );
+                System.out.println( "COMMAND " );
             }
-            else if (node instanceof AstWorkflow)
+            else if( node instanceof AstWorkflow )
             {
-                AstWorkflow workflowNode = ((AstWorkflow)node);
-                System.out.println( "WORKFLOW " + workflowNode.getName()); 
+                AstWorkflow workflowNode = ( (AstWorkflow)node );
+                System.out.println( "WORKFLOW " + workflowNode.getName() );
             }
-            else if (node instanceof AstCall)
+            else if( node instanceof AstCall )
             {
-                AstCall callNode = ((AstCall)node);
+                AstCall callNode = ( (AstCall)node );
                 AstSymbol[] inputs = callNode.getInputs();
-                System.out.println( "CALL " + callNode.getName()+" as "+ callNode.getAlias());
-                for (AstSymbol input: inputs)
+                System.out.println( "CALL " + callNode.getName() + " as " + callNode.getAlias() );
+                for( AstSymbol input : inputs )
                     System.out.println( input.toString() );
             }
-            else if (node instanceof AstBashString)
+            else if( node instanceof AstBashString )
             {
-                AstBashString bashNode = ((AstBashString)node);
-                System.out.println( "BASH " + bashNode.getCommand()); 
+                AstBashString bashNode = ( (AstBashString)node );
+                System.out.println( "BASH " + bashNode.getCommand() );
             }
-            else if (node instanceof AstRuntime)
+            else if( node instanceof AstRuntime )
             {
                 AstRuntime runtimeNode = (AstRuntime)node;
-                System.out.println( runtimeNode.toString());
+                System.out.println( runtimeNode.toString() );
             }
-            
-            
-            for (int i=0; i< node.jjtGetNumChildren(); i++ )
+
+
+            for( int i = 0; i < node.jjtGetNumChildren(); i++ )
             {
-                visitNode( (SimpleNode)node.jjtGetChild( i ));
+                visitNode( (SimpleNode)node.jjtGetChild( i ) );
             }
         }
     }
