de3dbdc884a189f5bdfd6bf153f00f18bcb14d8e
Ilya <damagedone@yandex.ru>
2025-09-18 18:13:58 +0400

    bean info for physicell emodel

---
+++ src/biouml/plugins/physicell/MulticellEModelBeanInfo.java
@@ -0,0 +1,16 @@
+package biouml.plugins.physicell;
+
+import ru.biosoft.util.bean.BeanInfoEx2;
+
+public class MulticellEModelBeanInfo extends BeanInfoEx2<MulticellEModel>
+{
+    public MulticellEModelBeanInfo()
+    {
+        super(  MulticellEModel.class );
+    }
+
+    @Override
+    public void initProperties()
+    {
+    }
+}
