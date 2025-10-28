a1cd20bbe9ca48ae89684ae950b49d7d8b69c0d9
Ilya <damagedone@yandex.ru>
2025-10-28 16:33:32 +0400

    ternary operator processed to nextflow form

--- src/biouml/plugins/wdl/NextFlowPreprocessor.java
+++ src/biouml/plugins/wdl/NextFlowPreprocessor.java
@@ -34,7 +34,8 @@ public class NextFlowPreprocessor
                 if( expression != null && !expression.isEmpty() )
                 {
                     String name = WorkflowUtil.getName( input );
-                    WorkflowUtil.addBeforeCommand( task, new Declaration( WorkflowUtil.getType( input ), name, "getDefault("+name+", "+expression+")"));
+                    WorkflowUtil.addBeforeCommand( task,
+                            new Declaration( WorkflowUtil.getType( input ), name, "getDefault(" + name + ", " + expression + ")" ) );
                 }
             }
 
@@ -57,6 +58,7 @@ public class NextFlowPreprocessor
             {
                 expression = processArrayElements( result, expression );
                 expression = removeGlobs( expression );
+                expression = processTernary( expression );
                 WorkflowUtil.setExpression( node, expression );
             }
         }
@@ -96,8 +98,9 @@ public class NextFlowPreprocessor
             String arrayName = matcher.group( 1 );
             String index = matcher.group( 2 );
 
-            Node arrayIndex = diagram.recursiveStream().select( Node.class ).filter(n->WorkflowUtil.isCycleVariable( n ) && WorkflowUtil.getName( n ).equals( index )).findAny().orElse( null );
-            if (arrayIndex != null)
+            Node arrayIndex = diagram.recursiveStream().select( Node.class )
+                    .filter( n -> WorkflowUtil.isCycleVariable( n ) && WorkflowUtil.getName( n ).equals( index ) ).findAny().orElse( null );
+            if( arrayIndex != null )
                 return input;
             String nextFlowStyle = arrayName + ".map{v->v[" + index + "]}";
             input = input.replace( matcher.group(), nextFlowStyle );
@@ -109,5 +112,22 @@ public class NextFlowPreprocessor
     {
         String regex = "glob\\((['\"])([a-zA-Z0-9./*_]+)\\1\\)";
         return input.replaceAll( regex, "\"$2\"" );
+    }
+
+    public static String processTernary(String ternary)
+    {
+        String patternString = "if\\s+(.+?)\\s+then\\s+(.+?)\\s+else\\s+(.+)";
+
+        Pattern pattern = Pattern.compile( patternString );
+        Matcher matcher = pattern.matcher( ternary );
+
+        if( matcher.matches() )
+        {
+            String condition = matcher.group( 1 ).trim();
+            String truePart = matcher.group( 2 ).trim();
+            String falsePart = matcher.group( 3 ).trim();
+            return condition +" ? " +truePart+" : "+falsePart;
+        }
+        return ternary;
     }
 }
