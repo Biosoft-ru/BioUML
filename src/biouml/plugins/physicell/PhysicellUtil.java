9ed5dda6a8d8edea81b2fbd94786df8cabcaa77d
Ilya <damagedone@yandex.ru>
2025-11-06 10:28:46 +0400

    hack for covid model legend

--- src/biouml/plugins/physicell/PhysicellUtil.java
+++ src/biouml/plugins/physicell/PhysicellUtil.java
@@ -6,7 +6,10 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.developmentontheedge.beans.DynamicProperty;
 
@@ -58,38 +61,78 @@ public class PhysicellUtil
         return null;
     }
 
+    
+    private static void hack4CovidModel(CellDefinition cd, Model model, Map<String, Cell> cells)
+    {
+        if( cd.name.equals( "macrophage" ) )
+        {
+            Cell c2 = new Cell( cd, model, false );
+            c2.customData.set( "activated_immune_cell", 1 );
+            cells.put( cd.name + " activated", c2 );
+
+            Cell c3 = new Cell( cd, model, false );
+            c3.phenotype.volume.total = c3.customData.get( "threshold_macrophage_volume" ) + 10;
+            cells.put( cd.name + " exhausted", c3 );
+
+            Cell c4 = new Cell( cd, model, false );
+            c4.customData.set( "ability_to_phagocytose_infected_cell", 1 );
+            cells.put( cd.name + " hyperactivated", c4 );
+        }
+        else if( cd.name.equals( "DC" ) )
+        {
+            Cell c2 = new Cell( cd, model, false );
+            c2.customData.set( "activated_immune_cell", 1 );
+            cells.put( "Dendrite cells activated", c2 );
+        }
+        else if( cd.name.equals( "lung epithelium" ) )
+        {
+            Cell c2 = new Cell( cd, model, false );
+            int color_index = c2.customData.findVariableIndex( c2.getModel().getParameterString( "color_variable" ) );
+            c2.customData.set( color_index, 750 );
+            cells.put( cd.name + " infected", c2 );
+        }
+    }
+    
     public static BufferedImage generateLegend(Model model, ColorFont font, AgentColorer colorer)
     {
         int yOffset = 10;
         int xOffset = 10;
         int radius = 10;
-        int nuclearRadius = 5;
         AgentVisualizer agentVisualizer = new AgentVisualizer();
         agentVisualizer.setAgentColorer( colorer );
         BufferedImage temp = new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB );
         Graphics gTemp = temp.getGraphics();
-        List<String> names = new ArrayList<>();
+
+        TreeMap<String, Cell> cells = new TreeMap<>();
         for( CellDefinition cd : model.getCellDefinitions() )
-            names.add( cd.name );
+        {
+            Cell c = new Cell( cd, model, false );
+            if( cd.name.equals( "DC" ) )
+                cells.put( "Dendrite cells", c );
+            else
+                cells.put( cd.name, c );
+            hack4CovidModel( cd, model, cells );
+        }
 
         FontMetrics fm = gTemp.getFontMetrics( font.getFont() );
 
-        int width = StreamEx.of( names ).mapToInt( s -> fm.stringWidth( s ) ).max().orElse( 0 );
-        int lineHeight = (int)Math.round(fm.getStringBounds("G", gTemp).getHeight());
-        int height = ( lineHeight + yOffset ) * names.size();
+        int width = StreamEx.of( cells.keySet() ).mapToInt( s -> fm.stringWidth( s ) ).max().orElse( 0 );
+        int lineHeight = (int)Math.round( fm.getStringBounds( "G", gTemp ).getHeight() );
+        int height = ( lineHeight + yOffset ) * cells.size();
 
-        BufferedImage img = new BufferedImage( width + xOffset + radius*2, height, BufferedImage.TYPE_INT_ARGB );
+        BufferedImage img = new BufferedImage( width + xOffset + radius * 2, height, BufferedImage.TYPE_INT_ARGB );
         Graphics g = img.getGraphics();
         int x = 0;
         int y = 0;
-        for( CellDefinition cd : model.getCellDefinitions() )
+
+
+        for( String name : cells.keySet() )
         {
-            String name = cd.name;
-            Cell c = new Cell( cd, model , false);
-            agentVisualizer.drawAgent( c, x + radius, y + radius, radius, nuclearRadius, g );
+            Cell c = cells.get( name );
+            agentVisualizer.drawAgent( c, x + radius, y + radius, (int)c.getRadius(), (int)c.phenotype.geometry.getNuclearRadius(), g );
             g.setFont( font.getFont() );
             g.setColor( Color.black );
-            g.drawString( name, x + 2 * radius + xOffset, y + radius  + lineHeight/2 );
+            g.drawString( name, x + 2 * radius + xOffset, y + radius + lineHeight / 2 );
             y += ( fm.getHeight() + yOffset );
         }
         return img;
