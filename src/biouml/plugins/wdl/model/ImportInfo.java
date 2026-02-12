7b5a0906d22bc1ebed9be04027f982ec761fa405
Ilya <axec@developmentontheedge.com>
2026-02-12 20:39:23 +0400

    ScriptInfo included into ImportInfo

--- src/biouml/plugins/wdl/model/ImportInfo.java
+++ src/biouml/plugins/wdl/model/ImportInfo.java
@@ -2,13 +2,30 @@ package biouml.plugins.wdl.model;
 
 public class ImportInfo
 {
+    private ScriptInfo imported;
     private String source;
     private String alias;
+    private String task;
 
+    public void ImportInfo()
+    {
+        
+    }
+    
     public ImportInfo(String alias, String source)
     {
         this.source = source;
         this.alias = alias;
+    }
+    
+    public String getTask()
+    {
+        return task;
+    }
+
+    public void setTask(String task)
+    {
+        this.task = task;
     }
     
     public String getSource()
@@ -16,8 +33,23 @@ public class ImportInfo
         return source;
     }
     
+    public void setSource(String source)
+    {
+        this.source = source;
+    }
+    
     public String getAlias()
     {
         return alias;
+    }
+    
+    public ScriptInfo getImported()
+    {
+        return imported;
+    }
+    
+    public void setImported(ScriptInfo scriptInfo)
+    {
+        this.imported = scriptInfo;
     }
 }
