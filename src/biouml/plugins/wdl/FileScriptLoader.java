a34a90ef7350ee08286399298f9323377a54a4d5
Ilya <axec@developmentontheedge.com>
2026-05-20 18:56:50 +0400

    hierarchical script loading improved

--- src/biouml/plugins/wdl/FileScriptLoader.java
+++ src/biouml/plugins/wdl/FileScriptLoader.java
@@ -6,32 +6,29 @@ import java.nio.file.Path;
 import com.developmentontheedge.application.ApplicationUtils;
 
 import biouml.plugins.wdl.model.ScriptInfo;
-import biouml.plugins.wdl.nextflow.NextFlowImporter;
 
-public class FileScriptLoader implements ScriptLoader
+public class FileScriptLoader extends ScriptLoader
 {
     public File roodDir;
 
-    public FileScriptLoader(File rootDir)
+    public FileScriptLoader(String type, File rootDir)
     {
+        super(type);
         this.roodDir = rootDir;
     }
 
     @Override
     public ScriptInfo loadScript(String path) throws Exception
     {
-        //        String rootPath = roodDir.getAbsolutePath();
         Path rootPath = roodDir.toPath();
         Path resultPath = rootPath.resolve( Path.of( path ) );
 
         File resultFile = resultPath.toFile();
 
-        String result = ApplicationUtils.readAsString( resultFile );
+        String script = ApplicationUtils.readAsString( resultFile );
         String name = resultFile.getName();
         name = name.substring( 0, name.indexOf( "." ) );
-        ScriptInfo importedScript = new NextFlowImporter().parseNextflow( name, result );
+        ScriptInfo importedScript = this.readScript( name, script );
         return importedScript;
-        //        File scriptFile = rootDir.
     }
-
 }
