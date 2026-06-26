49475ace48f48faabe15cb1d3f0bbf1fd530f148
Sergey Zhatchenko <shatchenko@gmail.com>
2026-06-26 15:05:06 +0700

    feat: use collapsed as default output format for monitoring

--- src/biouml/plugins/servermonitor/MonitoringService.java
+++ src/biouml/plugins/servermonitor/MonitoringService.java
@@ -219,7 +219,7 @@ public class MonitoringService {
             return new ProfilerResult("Max concurrent profiles reached");
         }
 
-        ProfilerResult result = profiler.start(new long[0], "traces");
+        ProfilerResult result = profiler.start(new long[0], "collapsed");
 
         if (result.isSuccess()) {
             activeProfiles.put("jvm", result);
